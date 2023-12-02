ee36960549eb2c36645e2479b0d15030c6e41634
Logan <lf222yv@student.lnu.se>
2023-12-01 20:02:32 -0500

    fixed reactivness

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Navbar elements={NavbarElements} />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
